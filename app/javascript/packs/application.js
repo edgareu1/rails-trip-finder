531c298a7ea812524acbf83cf9db19d6e54b7e99
Edgar Marques <edgar.eu1@hotmail.com>
2020-11-27 21:59:18 +0000

    Prepare JS folders

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,13 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// External imports
+import "bootstrap";
+
+// Internal imports
+// import { initSelect2 } from '../components/init_select2';
+
+document.addEventListener('turbolinks:load', () => {
+  // initSelect2();
+});
